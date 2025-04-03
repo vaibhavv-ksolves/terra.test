a0a0d5025052bfc505ad029335443ef8879e487e
vaibhavv-ksolves <vaibhav.vardhiya@ksolves.com>
2025-04-04 00:30:01 +0530

    Update variables.tf

--- eks_moudle/variables.tf
+++ eks_moudle/variables.tf
@@ -266,7 +266,7 @@ variable "kms_key_aliases" {
 variable "create_cloudwatch_log_group" {
   description = "Determines whether a log group is created by this module for the cluster logs. If not, AWS will automatically create one if logging is enabled"
   type        = bool
-  default     = false
+  default     = true
 }
 
 variable "cloudwatch_log_group_retention_in_days" {
