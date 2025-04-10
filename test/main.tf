a7d35575e57bf8aa8d0e74bdcf8dded413bb74e4
vaibhavv-ksolves <vaibhav.vardhiya@ksolves.com>
2025-04-10 10:53:19 +0530

    Update main.tf

--- test/main.tf
+++ test/main.tf
@@ -1,4 +1,4 @@
-terraform {
+	terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -42,210 +42,60 @@ module "vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  create_igw            = true
-  enable_nat_gateway    = true
-  single_nat_gateway    = false
+  create_igw             = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
   one_nat_gateway_per_az = true
 
-  manage_default_security_group = false
-  manage_default_network_acl    = false
+  manage_default_security_group  = false
+  manage_default_network_acl     = false
 
-  enable_flow_log                       = true
+  enable_flow_log                      = true
   create_flow_log_cloudwatch_log_group = true
   create_flow_log_cloudwatch_iam_role  = true
-  flow_log_destination_type             = "cloud-watch-logs"
+  flow_log_destination_type            = "cloud-watch-logs"
 
   tags = var.tags
 }
 
 # -----------------------------------------------------------------------------
-# VPC ENDPOINTS MODULE
+# IAM Policy Documents
 # -----------------------------------------------------------------------------
 
-module "vpc_endpoints" {
-  source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-  version = "~> 5.1.1"
+data "aws_iam_policy_document" "eks_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
 
-  vpc_id = module.vpc.vpc_id
-
-  endpoints = {
-    s3 = {
-      service         = "s3"
-      service_type    = "Gateway"
-      route_table_ids = module.vpc.private_route_table_ids
-      tags = {
-        Name = "s3-endpoint"
-      }
-    }
-
-    dynamodb = {
-      service         = "dynamodb"
-      service_type    = "Gateway"
-      route_table_ids = module.vpc.private_route_table_ids
-      tags = {
-        Name = "dynamodb-endpoint"
-      }
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
     }
   }
+}
 
-  tags = var.tags
+data "aws_iam_policy_document" "node_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
 
 # -----------------------------------------------------------------------------
-# IAM ROLE FOR EKS CLUSTER
+# IAM Roles for EKS
 # -----------------------------------------------------------------------------
 
 resource "aws_iam_role" "eks_cluster_role" {
-  name = "${var.eks_cluster_name}-cluster-role"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "eks.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-
-  tags = var.tags
+  name               = "${var.eks_cluster_name}-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
 }
 
-resource "aws_iam_policy_attachment" "eks_cluster_policy_attachment" {
-  name       = "eks-cluster-policy-attachment"
+resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  roles      = [aws_iam_role.eks_cluster_role.name]
-}
-
-resource "aws_iam_policy_attachment" "eks_vpc_cni_policy_attachment" {
-  name       = "eks-vpc-cni-policy-attachment"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  roles      = [aws_iam_role.eks_cluster_role.name]
-}
-
-# -----------------------------------------------------------------------------
-# IAM ROLE FOR EKS NODE GROUP
-# -----------------------------------------------------------------------------
-
-resource "aws_iam_role" "eks_node_group_role" {
-  name = "${var.eks_cluster_name}-nodegroup-role"
-
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-
-  tags = var.tags
-}
-
-resource "aws_iam_policy_attachment" "eks_node_group_policy_attachment" {
-  name       = "eks-node-group-policy-attachment"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  roles      = [aws_iam_role.eks_node_group_role.name]
-}
-
-resource "aws_iam_policy_attachment" "eks_node_group_cni_policy_attachment" {
-  name       = "eks-node-group-cni-policy-attachment"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  roles      = [aws_iam_role.eks_node_group_role.name]
-}
-
-resource "aws_iam_policy_attachment" "eks_node_group_ecr_readonly_policy_attachment" {
-  name       = "eks-node-group-ecr-readonly-policy-attachment"
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  roles      = [aws_iam_role.eks_node_group_role.name]
-}
-
-# -----------------------------------------------------------------------------
-# EKS SECURITY GROUP
-# -----------------------------------------------------------------------------
-
-resource "aws_security_group" "eks_cluster_sg" {
-  name_prefix = "${var.eks_cluster_name}-cluster-sg-"
-  vpc_id      = module.vpc.vpc_id
-
-  tags = var.tags
-}
-
-resource "aws_security_group_rule" "eks_cluster_sg_allow_https_from_public" {
-  type             = "ingress"
-  protocol         = "tcp"
-  from_port        = 443
-  to_port          = 443
-  cidr_blocks      = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.eks_cluster_sg.id
-}
-
-resource "aws_security_group_rule" "eks_cluster_sg_allow_kubelet_from_nodes" {
-  type             = "ingress"
-  protocol         = "tcp"
-  from_port        = 10250
-  to_port          = 10250
-  security_group_id = aws_security_group.eks_cluster_sg.id
-  source_security_group_id = aws_security_group.eks_node_group_sg.id
-}
-
-resource "aws_security_group_rule" "eks_cluster_sg_allow_control_plane_to_nodes" {
-  type             = "ingress"
-  protocol         = "tcp"
-  from_port        = 1025-65535
-  to_port          = 65535
-  security_group_id = aws_security_group.eks_cluster_sg.id
-  source_security_group_id = aws_security_group.eks_node_group_sg.id
-}
-
-resource "aws_security_group_rule" "eks_cluster_sg_allow_all_outbound" {
-  type             = "egress"
-  protocol         = "-1"
-  from_port        = 0
-  to_port          = 0
-  cidr_blocks      = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.eks_cluster_sg.id
-}
-
-# -----------------------------------------------------------------------------
-# EKS NODE GROUP SECURITY GROUP
-# -----------------------------------------------------------------------------
-
-resource "aws_security_group" "eks_node_group_sg" {
-  name_prefix = "${var.eks_cluster_name}-nodegroup-sg-"
-  vpc_id      = module.vpc.vpc_id
-
-  tags = var.tags
-}
-
-resource "aws_security_group_rule" "eks_node_group_sg_allow_kubelet_from_control_plane" {
-  type             = "ingress"
-  protocol         = "tcp"
-  from_port        = 1025-65535
-  to_port          = 65535
-  security_group_id = aws_security_group.eks_node_group_sg.id
-  source_security_group_id = aws_security_group.eks_cluster_sg.id
-}
-
-resource "aws_security_group_rule" "eks_node_group_sg_allow_ssh" {
-  type        = "ingress"
-  protocol    = "tcp"
-  from_port   = 22
-  to_port     = 22
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.eks_node_group_sg.id
-}
-
-resource "aws_security_group_rule" "eks_node_group_sg_allow_all_outbound" {
-  type             = "egress"
-  protocol         = "-1"
-  from_port        = 0
-  to_port          = 0
-  cidr_blocks      = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.eks_node_group_sg.id
+  role       = aws_iam_role.eks_cluster_role.name
 }
 
 # -----------------------------------------------------------------------------
@@ -253,138 +103,157 @@ resource "aws_security_group_rule" "eks_node_group_sg_allow_all_outbound" {
 # -----------------------------------------------------------------------------
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.0"
-
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 19.0"
   cluster_name    = var.eks_cluster_name
   cluster_version = var.eks_cluster_version
-  cluster_endpoint_public_access = true
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  vpc_id    = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
-  cluster_security_group_id = aws_security_group.eks_cluster_sg.id
+  # Use pre-created IAM role
+  create_iam_role = false
+  iam_role_arn    = aws_iam_role.eks_cluster_role.arn
 
   eks_managed_node_groups = {
-    ng1 = {
-      name           = "default-node-group"
-      instance_types = ["t3.medium"]
-      desired_size   = 2
+    default = {
       min_size       = 1
       max_size       = 3
+      desired_size   = 2
+      instance_types = ["t3.medium"]
 
-      subnet_ids = module.vpc.private_subnets
-
-      node_group_role_arn = aws_iam_role.eks_node_group_role.arn
-      node_security_group_ids = [aws_security_group.eks_node_group_sg.id]
-
-      tags = var.tags
+      # IAM role for nodes
+      create_iam_role      = true
+      iam_role_name        = "${var.eks_cluster_name}-node-role"
+      iam_role_use_name_prefix = false
+      iam_role_description = "EKS node group role"
+      iam_role_additional_policies = {
+        AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+        AmazonEC2ContainerRegistryReadOnly = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+      }
     }
   }
 
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
+
   tags = var.tags
 }
-
 # -----------------------------------------------------------------------------
-# KUBECONFIG OUTPUT
-# -----------------------------------------------------------------------------
-
-output "kubeconfig" {
-  value = yamlencode({
-    apiVersion = "v1"
-    kind       = "Config"
-    clusters = [{
-      name = var.eks_cluster_name
-      cluster = {
-        server                   = module.eks.cluster_endpoint
-        certificate-authority-data = module.eks.cluster_certificate_authority_data
-      }
-    }]
-    users = [{
-      name = "aws"
-      user = {
-        exec = {
-          apiVersion = "client.authentication.k8s.io/v1beta1"
-          command    = "aws"
-          args = [
-            "eks", "get-token",
-            "--cluster-name", var.eks_cluster_name
-          ]
-        }
-      }
-    }]
-    contexts = [{
-      name    = "aws"
-      context = {
-        cluster = var.eks_cluster_name
-        user    = "aws"
-      }
-    }]
-    current-context = "aws"
-  })
-}
-
-# -----------------------------------------------------------------------------
-# PROVIDER CONFIGURATION FOR HELM & K8S
+# Kubernetes Provider Configuration
 # -----------------------------------------------------------------------------
 
 provider "kubernetes" {
-  alias                  = "eks"
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec = {
-    apiVersion = "client.authentication.k8s.io/v1beta1"
-    command    = "aws"
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
     args = [
-      "eks", "get-token",
-      "--cluster-name", var.eks_cluster_name
+      "eks",
+      "get-token",
+      "--cluster-name",
+      module.eks.cluster_name,
+      "--region",
+      var.aws_region
     ]
   }
 }
 
-provider "helm" {
-  alias = "eks"
+# -----------------------------------------------------------------------------
+# Helm Provider Configuration
+# -----------------------------------------------------------------------------
 
+provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
+    
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args = [
-        "eks", "get-token",
-        "--cluster-name", var.eks_cluster_name
+        "eks",
+        "get-token",
+        "--cluster-name",
+        module.eks.cluster_name,
+        "--region",
+        var.aws_region
       ]
     }
   }
 }
 
 # -----------------------------------------------------------------------------
-# NGINX INGRESS CONTROLLER INSTALLATION
+# AWS Auth ConfigMap (Updated with correct node group reference)
+# -----------------------------------------------------------------------------
+
+
+resource "kubernetes_config_map_v1" "eks_aws_auth" {
+  metadata {
+    name      = "eks-aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapRoles = yamlencode([
+      {
+        rolearn  = module.eks.eks_managed_node_groups["default"].iam_role_arn
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = ["system:bootstrappers", "system:nodes"]
+      },
+      {
+        rolearn  = aws_iam_role.eks_cluster_role.arn
+        username = "eks-admin"
+        groups   = ["system:masters"]
+      }
+    ])
+  }
+
+  depends_on = [module.eks]
+}
+
+# -----------------------------------------------------------------------------
+# NGINX INGRESS CONTROLLER (with updated dependency)
 # -----------------------------------------------------------------------------
 
 resource "helm_release" "nginx_ingress" {
-  provider    = helm.eks
-  name        = "nginx-ingress"
-  namespace   = "kube-system"
-  repository  = "https://kubernetes.github.io/ingress-nginx"
-  chart       = "ingress-nginx"
-  version     = "4.10.0"
+  name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "4.0.18"
+  namespace  = "kube-system"
 
-  values = [
-    yamlencode({
-      controller = {
-        replicaCount = 2
-        service = {
-          type = "LoadBalancer"
-          annotations = {
-            "service.beta.kubernetes.io/aws-load-balancer-type" = "nlb"
-          }
-        }
-      }
-    })
+  set {
+    name  = "controller.service.type"
+    value = "LoadBalancer"
+  }
+
+  depends_on = [
+    kubernetes_config_map_v1.eks_aws_auth  # Updated reference
   ]
+}
 
-  depends_on = [module.eks]
+# -----------------------------------------------------------------------------
+# OUTPUTS (Updated with correct node group reference)
+# -----------------------------------------------------------------------------
+
+output "eks_cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "eks_cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
+output "eks_node_group_role_arn" {
+  value = module.eks.eks_managed_node_groups["default"].iam_role_arn
+}
+
+output "eks_oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
 }
